2fb3bfdd4d70f8a01a764bd97427dc98078f3af3
Misbaulhuda <misbahuda912@gmail.com>
2026-06-04 12:59:29 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-def load_data():
-    return pd.read_csv(
-        "data/Teen_Mental_Health_Dataset.csv"
-    )
+df = pd.read_csv("data/Teen_Mental_Health_Dataset.csv")
+
+print(df.head())
